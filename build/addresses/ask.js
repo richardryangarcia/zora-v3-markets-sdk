5dfd5098141e8b65cd245f1bd8fd10789657756f
Richard Ryan <richardryangarcia@gmail.com>
2022-09-14 19:22:13 -0500

    added goerli v3 ask address

--- build/addresses/ask.js
+++ build/addresses/ask.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     1: "0x6170B3C3A54C3d8c854934cBC314eD479b2B29A3",
     4: "0xA98D3729265C88c5b3f861a0c501622750fF4806",
-    5: "0x4Ae7072bbfF93F452855d32Bf90E1230Bf7deB26",
+    5: "0xd8be3E8A8648c4547F06E607174BAC36f5684756",
     137: "0x3634e984Ba0373Cfa178986FD19F03ba4dD8E469",
     80001: "0xCe6cEf2A9028e1C3B21647ae3B4251038109f42a", // polygon mumbai
 };
--- src/addresses/ask.ts
+++ src/addresses/ask.ts
@@ -1,7 +1,7 @@
 export default {
   1: "0x6170B3C3A54C3d8c854934cBC314eD479b2B29A3", // ethereum mainnet
   4: "0xA98D3729265C88c5b3f861a0c501622750fF4806", //  god damn rinkeby
-  5: "0x4Ae7072bbfF93F452855d32Bf90E1230Bf7deB26", // ethereum goerli module not registered smh
+  5: "0xd8be3E8A8648c4547F06E607174BAC36f5684756", // ethereum goerli
   137: "0x3634e984Ba0373Cfa178986FD19F03ba4dD8E469", // polygon mainnet,
   80001: "0xCe6cEf2A9028e1C3B21647ae3B4251038109f42a", // polygon mumbai
 } as { [chainId: number]: string };
